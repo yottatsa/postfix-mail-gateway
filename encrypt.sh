de074374eb2319ff3f1a1f7f948d9cc0a0ca5318
Vladimir Eremin <me@yottatsa.name>
2017-03-07 14:50:04 +0000

    rnd state

--- encrypt.sh
+++ encrypt.sh
@@ -5,6 +5,7 @@ TO="$4"
 CERT="/etc/postfix/smime.crt"
 SENDMAIL="/usr/sbin/sendmail -G -i" # NEVER NEVER NEVER use "-t" here.
 OPENSSL="/usr/bin/openssl smime -encrypt -from ${FROM} -to ${TO} -subject encrypted -aes256 ${CERT}"
+export RANDFILE="/tmp/encrypt.rnd"
 
 if [ -e "${CERT}" ]
 then
