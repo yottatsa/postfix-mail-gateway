4f5d31fa916165eec69371d75b5496bb29720257
Vladimir Eremin <yottatsa@fb.com>
2017-11-08 20:23:04 +0000

    security

--- encrypt.sh
+++ encrypt.sh
@@ -1,17 +1,23 @@
 #!/bin/sh -e
 
-FROM="$2"
-TO="$4"
+SIGNCERT="/etc/postfix/ssl/cert.pem"
+SIGNKEY="/etc/postfix/ssl/privkey.pem"
 CERT="/etc/postfix/smime.crt"
+SERVERSIGNED="=?utf-8?B?8J+UkA==?="
+
 SENDMAIL="/usr/sbin/sendmail -G -i" # NEVER NEVER NEVER use "-t" here.
-OPENSSL="/usr/bin/openssl smime -encrypt -from ${FROM} -to ${TO} -subject encrypted -aes256 ${CERT}"
 export RANDFILE="/tmp/encrypt.rnd"
+
+FROM="$2" 
+TO="$4"
 
 if [ -e "${CERT}" ]
 then
-	$OPENSSL | $SENDMAIL "$@"
+        /usr/bin/openssl smime -sign -signer "${SIGNCERT}" -inkey "${SIGNKEY}" |\
+        /usr/bin/openssl smime -encrypt -aes256 -from "${FROM}" -to "${TO}" -subject "${SERVERSIGNED}" "${CERT}" |\
+        $SENDMAIL "$@"
 else
-	$SENDMAIL "$@"
+        $SENDMAIL "$@"
 fi
 
 exit $?
